198dea99526c79fc57ed0ce00cac619f72d0c4e2
xaizan100 <xaizan.ahmad@gmail.com>
2025-11-22 05:26:27 +0000

    Home Dashboard

---
+++ Graphene/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Graphene.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
